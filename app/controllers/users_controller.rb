4173e5a6fc91bc32291a86ca6cb1811b29e53c45
Andrey Krahmal <andrey@fundwise.com>
2016-10-14 14:07:28 +0100

    tweaks and making gibbon work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    mailchimp_register(params[:name], params[:email])   
+    mailchimp_register(params[:name], params[:email], params[:phone], params[:message], params[:country], params[:contact])   
   end
 
-  def mailchimp_register(name, email)
+  def mailchimp_register(name, email, phone, message, country, contact)
   	gb = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
     begin
 
-    	gb.lists.members.create(body: {
+    	gb.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: {
           :email_address => email, 
           :status => "subscribed", 
-          :merge_fields => {FNAME: name},
-          :double_optin => false,
-          :send_welcome => true})
+          :merge_fields => {FNAME: name, PHONE: phone, MESSAGE: message, COUNTRY: country, CONTACT: contact }})
 
 		rescue Gibbon::MailChimpError => e
-      error = e.code == 214 ? "#{email} is already subscribed" : e.message
+      error = e.status_code == 214 ? "#{email} is already subscribed" : e.message
   		flash[:error] = error
       redirect_to new_user_path
     else
