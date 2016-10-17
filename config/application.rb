4d271ffd97b4bef1b620aeb7409c7e14503eeb78
Andrey Krahmal <andrey@fundwise.com>
2016-10-17 13:03:29 +0100

    require rails all

--- config/application.rb
+++ config/application.rb
@@ -4,8 +4,11 @@ require_relative 'boot'
 
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
+require 'action_view/railtie'
 require 'active_model/railtie'
+require 'active_job/railtie'
 require 'sprockets/railtie'
+require 'action_cable/engine'
 require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
